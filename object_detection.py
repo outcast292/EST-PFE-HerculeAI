41b3f4fadc76b8ec09f964c8c370879de888efe4
Hatim Ha <hatimgbl@gmail.com>
2020-03-02 17:17:46 +0000

    object_detection.py edited online with Bitbucket

--- object_detection.py
+++ object_detection.py
@@ -7,14 +7,20 @@ import sys
 
 execution_path = os.getcwd()
 detected = []
-global camera
+
 resized = False
 
 def forSecond(frame_number, output_arrays, count_arrays, average_count, returned_frame):
-    plt.subplot(1, 1, 1)
-    plt.axis("off")
-    plt.imshow(returned_frame, interpolation="none")
-
+    global detected
+    if len(output_arrays) != 0:
+        for eachItem in output_arrays:
+            dictx = {
+                "name" : eachItem[0].get("name"),
+                "box_points" : eachItem[0].get("box_points"),
+            }
+            detected.append(dictx)
+    else:
+        print("empty")
 
 
 
@@ -24,20 +30,21 @@ def launch_detector():
     video_detector.setModelPath(os.path.join(execution_path, "yolo.h5")) # Download the model via this link https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0
     video_detector.loadModel()
 
-    camera    =  cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    camera = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
-    costum_obj = video_detector.CustomObjects(cup = True, orange=True)
+    costum_obj = video_detector.CustomObjects(person = True)
     video_detector.detectCustomObjectsFromVideo(camera_input=camera, 
                                             save_detected_video=False,  
-                                            frames_per_second=20,
-                                            frame_detection_interval=20, 
+                                            frames_per_second=1,
+                                            frame_detection_interval=1, 
                                             per_second_function=forSecond,  
                                             minimum_percentage_probability=30, 
                                             return_detected_frame=True, 
                                             log_progress=True,
-                                            detection_timeout=2,
+                                            detection_timeout=10,
                                             display_percentage_probability=False,
-                                            custom_objects=costum_obj  )
+                                            custom_objects=costum_obj)
+
     camera.release()
     cv2.destroyAllWindows()
 
