c080f58df6bb908b6c74e19dccf3750854f23eb8
Hatim Ha <hatimgbl@gmail.com>
2020-03-07 12:17:33 +0000

    launch script

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
+import object_detection
 
 
-
-
-if obj_detect == 'banane':
-    pickup()
-
-    if picked_up is True: 
-        moveToDropLocation(banane)
-        if arrivedAtDropLocation is True:
-            drop()
-    else 
-        pickup()
-
-if obj_detect == 'orange':
-    pickup()
-
-    if picked_up is True: 
-        moveToDropLocation(orange)
-        if arrivedAtDropLocation is True:
-            drop()
-    else 
-        pickup()
-
-
-if obj_detect == 'apple':
-    pickup()
-
-    if picked_up is True: 
-        moveToDropLocation(apple)
-        if arrivedAtDropLocation is True:
-            drop()
-    else 
-        pickup()
-
+object_detection.launch_detector()
